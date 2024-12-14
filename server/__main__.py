7b6bb8accf44d74f9674f6273bc582992b2bb512
amogus <wipocket@protonmail.com>
2024-12-14 01:19:02 +0100

    server init

---
+++ server/__main__.py
@@ -0,0 +1,21 @@
+from aiohttp import web
+import socketio
+
+sio = socketio.AsyncServer()
+app = web.Application()
+sio.attach(app)
+
+@sio.event
+def connect(sid, environ):
+    print("connect ", sid)
+
+@sio.event
+async def sex(sid, data):
+    print("sex ", data)
+
+@sio.event
+def disconnect(sid):
+    print('disconnect ', sid)
+
+if __name__ == '__main__':
+    web.run_app(app)

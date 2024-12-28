cbcc4c40cd68e5d36357f605f13e34cb1a471c90
chamik <jakub.hampl@email.cz>
2024-12-28 13:02:44 +0100

    disallow concurrent sessions with one account

--- server/main.ts
+++ server/main.ts
@@ -23,9 +23,11 @@ let users: { [gameToken: string]: User } = {
     }
 };
 
+let liveUsers = new Set<string>();
+
 let server;
 if (process.env.NODE_ENV === 'production') {
-    console.log('Starting in production mode: using HTTP');
+    console.log('Starting in production mode: using HTTP (secured with Caddy)');
     server = createHttpServer();
 } else {
     console.log('Starting in development mode: using HTTPS');
@@ -48,6 +50,12 @@ io.on('connection', (socket) => {
     const gameToken = socket.handshake.auth.gameToken || `User-${socket.id}`;
     console.log(`Connection attempt with code ${gameToken}`);
 
+    if (liveUsers.has(gameToken)) {
+        console.log(`${gameToken} already active`);
+        socket.emit('auth_error', 'You are already logged in on another device.');
+        return;
+    }
+
     let user: User | undefined = undefined;
     if (gameToken in users) {
         user = users[gameToken];
@@ -58,15 +66,25 @@ io.on('connection', (socket) => {
         return;
     }
 
+    // AUTHENTICATED
+
+    liveUsers.add(gameToken);
     socket.emit('user_data', user);
+
+    console.log('live users:')
+    liveUsers.entries().forEach(element => {
+        console.log(element);
+    });
 
     // Set up event listeners for authenticated users
     socket.on('fap', () => {
         users[gameToken].score += 1;
         socket.emit('user_data', user);
+        console.log(`${users[gameToken].name} fapped!`);
     });
 
     socket.on('disconnect', () => {
+        liveUsers.delete(gameToken);
         console.log(`User with ${gameToken} disconnected`);
     });
 });
